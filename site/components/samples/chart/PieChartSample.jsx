cec92a5eb7b8f64273a2f9790ae8358905a060fd
Recep Köseoğlu <recepkoseoglu@hotmail.com>
2017-01-26 12:13:06 +0300

    chart developed

--- site/components/samples/chart/PieChartSample.jsx
+++ site/components/samples/chart/PieChartSample.jsx
@@ -3,19 +3,86 @@ import {ShallowComponent} from "robe-react-commons";
 import PieChart from "robe-react-ui/lib/charts/PieChart";
 
 let data = [
-    {name: "Page A", uv: 4000, pv: 2400, amt: 2400, fill: "#8884d8"},
-    {name: "Page B", uv: 3000, pv: 1398, amt: 2210, fill: "#83a6ed"},
-    {name: "Page C", uv: 2000, pv: 9800, amt: 2290, fill: "#8dd1e1"},
-    {name: "Page D", uv: 2780, pv: 3908, amt: 2000, fill: "#82ca9d"},
-    {name: "Page E", uv: 1890, pv: 4800, amt: 2181, fill: "#a4de6c"},
-    {name: "Page F", uv: 2390, pv: 3800, amt: 2500, fill: "#d0ed57"},
-    {name: "Page G", uv: 3490, pv: 4300, amt: 2100, fill: "#ffc658"}
+    {
+        value: 4000,
+        label: "Robe",
+        unit: "ms",
+        fill: "#8884d8",
+        detail: [
+            {
+                value: 600,
+                label: "Robe",
+                unit: "ms",
+                fill: "#82ca9d"
+            },
+            {
+                value: 1000,
+                label: "UI",
+                unit: "ms",
+                fill: "#d0ed57"
+            },
+            {
+                value: 2400,
+                label: "Module",
+                unit: "ms",
+                fill: "#ffc658"
+            }
+        ]
+    },
+    {
+        value: 2400,
+        label: "React",
+        unit: "ms",
+        fill: "#83a6ed",
+        detail: [
+            {
+                value: 600,
+                label: "Components",
+                unit: "ms",
+                fill: "#82ca9d"
+            },
+            {
+                value: 1000,
+                label: "Docs",
+                unit: "ms",
+                fill: "#d0ed57"
+            },
+            {
+                value: 2400,
+                label: "Examples",
+                unit: "ms",
+                fill: "#ffc658"
+            }
+        ]
+    },
+    {
+        value: 2400,
+        label: "Npm",
+        unit: "ms",
+        fill: "#8dd1e1",
+        detail: [
+            {
+                value: 600,
+                label: "Install",
+                unit: "ms",
+                fill: "#82ca9d"
+            },
+            {
+                value: 1000,
+                label: "Test",
+                unit: "ms",
+                fill: "#d0ed57"
+            },
+            {
+                value: 2400,
+                label: "Start",
+                unit: "ms",
+                fill: "#ffc658"
+            }
+        ]
+    }
 ];
 
-let pies = [
-    {valueKey: "uv", data: data, cx: 200, cy: 200, outerRadius: 20, fill: "red", label: true},
-    // {valueKey: "pv", data: data, cx: 200, cy: 200, innerRadius: 100, outerRadius: 120, fill: "blue", label: true}
-]
 
 export default class PieChartSample extends ShallowComponent {
 
@@ -23,7 +90,7 @@ export default class PieChartSample extends ShallowComponent {
         return (
             <div>
                 <div className="form-group">
-                    <PieChart width={600} height={400} data={pies}/>
+                    <PieChart size={400} data={data}/>
                 </div>
             </div>
         );
--- src/charts/PieChart.jsx
+++ src/charts/PieChart.jsx
@@ -6,17 +6,15 @@ import Legend from "./Legend";
 export default class PieChart extends ShallowComponent {
 
     static propTypes = {
-        width: React.PropTypes.number,
-        height: React.PropTypes.number,
         data: React.PropTypes.array,
-        pies: React.PropTypes.array
+        pies: React.PropTypes.array,
+        size: React.PropTypes.number
     };
 
     static defaultProps = {
-        width: 500,
-        height: 300,
+        size: 400,
         data: [],
-        pies: []
+
     };
 
     constructor(props) {
@@ -26,90 +24,108 @@ export default class PieChart extends ShallowComponent {
     render() {
         return (
             <div id="pie" style={{margin:40}}>
-                <div className="rb-pie-chart" style={{width:this.props.width,height:this.props.height}}>
-                    <svg className="rb-pie-chart-layout" viewPort="0 0 100 100">
-                        {this.renderScatters(this.props.data, this.props.pies)}
+                <div className="rb-pie-chart" style={{width:this.props.size,height:this.props.size}}>
+                    <svg className="rb-pie-chart-layout">
+                        {this.renderScatters(this.props.data)}
                     </svg>
                 </div>
                 <br/>
                 <Legend
-                    width={this.props.width}
+                    width={this.props.size}
                     data={this.props.data}
                     chart="pie"
-                    legends={this.props.pies}/>
+                    legends={this.props.data}/>
             </div>
         )
     }
 
-    renderScatters(data, pies) {
-        let itemArr = [];
-        for (let i in data) {
-            let item = data[i];
-            let cx = item.cx;
-            let cy = item.cy;
-            let outerRadius = item.outerRadius;
-            let innerRadius = item.innerRadius;
-            let fill = item.fill;
+    renderScatters(data) {
+        let piesArr = [];
+        let sectors = this.calculateSectors(data);
 
-            for (let j in item.data) {
-                let child = item.data[j];
-                for (let key in child) {
-                    if (child.hasOwnProperty(key)) {
-                        if (key === "name" || key === "fill" || key === "unit") {
-                            continue;
-                        }
-                        itemArr.push(
-                            <circle
-                                cx={cx}
-                                cy={cy}
-                                r="190"
-                                className="rb-pie"
-                                style={{
-                                strokeDasharray:565.48,
-                                strokeDashoffset:0,
-                                stroke:"#FF9F1E",
-                                strokeWidth:3
-                                }}
-                                fill={"transparent"}/>);
-                    }
-                }
+        sectors.map(function (sector) {
+            piesArr.push(
+                <path
+                    className="rb-pie"
+                    transform={'rotate(' + sector.R + ', ' + sector.O + ', ' + sector.O + ')'}
+                    d={'M ' + sector.O + ' ' + sector.O + ' L ' + sector.Lx +' '+ sector.Ly + ' A ' + sector.L + ' ' + sector.L + ' 1 0 1 ' + sector.X + '  ' + sector.Y + ' z'}
+                    stroke={sector.color}
+                    fill={sector.color}/>
+            );
+        });
 
+        return piesArr;
+    }
+
+
+    __maxAxis(data, valueKey) {
+        let max = 0;
+        data.map(function (item, key) {
+            let value = item[valueKey];
+            max += value;
+        });
+        return max;
+    }
+
+    calculateSectors(pies) {
+        let sectors = [];
+
+        let aRad = 0; // Angle in Rad
+
+        let z = 0; // Size z
+        let x = 0; // Side x
+        let y = 0; // Side y
+
+        let X = 0; // SVG X coordinate
+        let Y = 0; // SVG Y coordinate
+
+        let radius = 100;
+        let origin = this.props.size / 2;
+
+        let rotation = 0;
+        let max = this.__maxAxis(pies, "value");
+
+        pies.map(function (item, key) {
+            let value = item.value;
+            let percentage = (360 * value) / max;
+            let aCalc = ( percentage > 180 ) ? 360 - percentage : percentage;
+
+            aRad = aCalc * Math.PI / 180;
+
+            z = Math.sqrt(2 * radius * radius - ( 2 * radius * radius * Math.cos(aRad) ));
+
+            if (aCalc <= 90) {
+                x = radius * Math.sin(aRad);
             }
-        }
-        return itemArr;
-    }
-
-
-    __maxAxis() {
-        let data = this.props.data;
-        let maxValue = 0;
-        for (let i in data) {
-            let item = data[i];
-            for (let j in item.data) {
-                let child = item.data[j];
-                for (let key in child) {
-                    if (child.hasOwnProperty(key)) {
-                        if (key === "name" || key === "fill" || key === "unit") {
-                            continue;
-                        }
-                        if (child[key] > maxValue)
-                            maxValue = child[key];
-                    }
-                }
+            else {
+                x = radius * Math.sin((180 - aCalc) * Math.PI / 180);
             }
-        }
-        let max = maxValue > 1000 ? 1000 : maxValue > 100 ? 100 : maxValue > 40 ? 40 : maxValue > 10 ? 10 : 1;
 
-        return (~~((maxValue + max - 1) / max) * max);
-    }
+            y = Math.sqrt(z * z - x * x);
+            Y = (origin - radius) + y;
 
-    __pieHeight(value) {
-        let max = this.__maxAxis();
-        return this.props.height - ((this.props.height * ((value * 100) / max)) / 100);
-    }
+            if (percentage <= 180) {
+                X = origin + x;
+            }
+            else {
+                X = origin - x;
+            }
+            sectors.push({
+                label: item.label,
+                color: item.fill,
+                L: radius,
+                O: origin,
+                X: X,
+                Y: Y,
+                Lx: origin,
+                Ly: origin - radius,
+                R: rotation
+            });
 
-    __pieWidth(value) {
-        let max = this.__maxAxis();
-        return ((this.props.width * ((value * 100) / max)) / 100);
+            rotation = rotation + percentage;
+        });
+
+
+        return sectors
     }
 }
